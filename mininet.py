c4a090995b4e418cb11a7ea8161931ac4ffc6b8a
teebana <t.balakumar@unsw.edu.au>
2020-07-24 17:09:29 +1000

    added mininet topology file

---
+++ mininet.py
@@ -0,0 +1,47 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import OVSController, RemoteController, CPULimitedHost
+from mininet.link import TCLink
+from mininet.nodelib import NAT
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.cli import CLI
+
+class ProjTopo(Topo):
+
+	def build(self, h = 1):
+		gatewayIP = '10.0.0.69' # establish a default gateway
+
+		# create NAT node for network
+		nat = self.addNode('nat', cls=NAT, ip=gatewayIP)
+
+		# edge switch to connect with NAT node
+		s1 = self.addSwitch('s1') # TODO: might have to put a DPID
+		self.addLink(s1, nat, port1=1, port2=1)
+
+
+		# consumer edge switch to connect to host
+		s2 = self.addSwitch('s2')
+		# connect consumer switch to NAT switch
+		self.addLink(s1, s2, port1=2, port2=2, bw=50) # generic link
+		self.addLink(s1, s2, port1=3, port2=3, bw=200) # high bandwidth link
+
+		# connect consumer switch to host
+		host = self.addHost('h1', ip='10.0.01', defaultRoute='via %s' % gatewayIP) # TODO: figure out what defaultRoute argument means
+		self.addLink(s2, host, port1=1)
+
+
+def simpleTest():
+
+	topo = ProjTopo(1)
+	natSubnet = '10.0.0.0/23'
+	net = Mininet(topo=topo, controller=None, autoSetMacs=True, autoStaticArp=True,ipBase=natSubnet)
+	net.addController('c0', controller=RemoteController, ip="127.0.0.1", port=6633, protocols="OpenFlow13")
+
+	net.start()
+
+	CLI(net)
+
+	net.stop()
+
+topos = { 'mytopo': (lambda: simpleTest() ) }
